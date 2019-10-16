67f18092ef15a2464e051ea883d23e2457c978d2
Alina Kornilova <alinakornilova78@gmail.com>
2019-10-16 01:28:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,4 +1,17 @@
-class Triangle
-  attr_accessor :equilateral, :isosceles, :scalene
-  def kind
+def triangle(a, b, c)
+  sides = [a,b,c].sort
+  raise TriangleError if sides.any? { |side| side <= 0 }
+  raise TriangleError unless (sides[0] + sides[1]) > sides[2]
+  sides.uniq!
+  if sides.count == 1
+    :equilateral
+  elsif sides.count == 2
+    :isosceles
+  else
+    :scalene
+  end
+end
+
+# Error class used in part 2.  No need to change this code.
+class TriangleError < StandardError
 end
