d610e17605a9a5ebacc1db1622b18d62d16292dc
Alina Kornilova <alinakornilova78@gmail.com>
2019-10-16 01:28:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,17 +1,40 @@
-def triangle(a, b, c)
-  sides = [a,b,c].sort
-  raise TriangleError if sides.any? { |side| side <= 0 }
-  raise TriangleError unless (sides[0] + sides[1]) > sides[2]
-  sides.uniq!
-  if sides.count == 1
-    :equilateral
-  elsif sides.count == 2
-    :isosceles
-  else
-    :scalene
+require 'pry'
+class Triangle
+  def initialize(side_1, side_2, side_3)
+    @triangle_sides = []
+    @triangle_sides << side_1
+    @triangle_sides << side_2
+    @triangle_sides << side_3
+  end
+
+  def valid?
+    sum_one_two = @triangle_sides[0] + @triangle_sides[1]
+    sum_one_three = @triangle_sides[0] + @triangle_sides[2]
+    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
+
+    if (@triangle_sides.none? {|side| side <= 0}) &&
+      (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
+      return true
+    else
+      return false
+    end
+  end
+
+  def kind
+    if valid?
+      if @triangle_sides.uniq.length == 1
+        return :equilateral
+      elsif @triangle_sides.uniq.length == 2
+        return :isosceles
+      else
+        return :scalene
+      end
+    else
+      raise TriangleError
+    end
   end
 end
 
-# Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
+
 end
